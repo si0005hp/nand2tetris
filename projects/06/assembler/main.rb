0f631278fae9eafb9762627e1f4869af0e9aac76
si0005hp <si0005hp@gmail.com>
2022-04-26 18:13:33 +0900

    ch06

---
+++ projects/06/assembler/main.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+if ARGV.empty?
+  puts '[ERROR]: no input files'
+  exit 1
+end
+
+def read_file(path)
+  File.read_file
+end
---
+++ projects/06/assembler/parser.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class Parser
+  def initialize(file)
+    @file = file
+  end
+end

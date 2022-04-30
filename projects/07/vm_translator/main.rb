16053568beb094d0886e0f8ef7ddeb3c6ac5e2e1
si0005hp <si0005hp@gmail.com>
2022-04-30 20:40:41 +0900

    ch07 (2)

--- projects/07/vm_translator/main.rb
+++ projects/07/vm_translator/main.rb
@@ -1,16 +1,39 @@
 # frozen_string_literal: true
 
 require './parser'
+require './code_writer'
 
 if ARGV.empty?
   puts '[ERROR]: no input files'
   exit 1
 end
 
-input_file_path = ARGV.first
+input_path = ARGV.first
 
-File.open(input_file_path, 'r') do |in_file|
-  parser = Parser.new(in_file)
+output_file_path, input_file_paths =
+  if input_path.end_with?('.vm')
+    # input is file
+    [input_path.gsub(/\.vm$/, '.asm'),
+     [input_path]]
+  else
+    # input is directory
+    ["#{input_path}/#{File.basename(input_path)}.asm",
+     Dir.glob("#{input_path}/**").select { |path| path.end_with?('.vm') }]
+  end
 
-  parser.advance while parser.has_more_commands?
+File.open(output_file_path, 'w') do |out_file|
+  writer = CodeWriter.new(out_file)
+
+  input_file_paths.each do |path|
+    File.open(path, 'r') do |in_file|
+      parser = Parser.new(in_file)
+      writer.file_name = File.basename(path).gsub(/\.vm$/, '')
+
+      while parser.has_more_commands?
+        next unless parser.advance
+
+        writer.write(parser.command, parser.arg1, parser.arg2)
+      end
+    end
+  end
 end
--- projects/07/vm_translator/parser.rb
+++ projects/07/vm_translator/parser.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Parser
-  attr_reader :command_type, :arg1, :arg2
+  attr_reader :command, :arg1, :arg2
 
   def initialize(file)
     @file = file
@@ -13,9 +13,10 @@ class Parser
 
   def advance
     line = remove_spaces_and_comment(@file.readline.chomp)
-    return if line.empty?
+    return false if line.empty?
 
     parse_line(line)
+    true
   end
 
   private
@@ -24,25 +25,12 @@ class Parser
     matched = /(\w+)(?: (\w+))?(?: (\w+))?$/.match(line)
     raise "[Parser error]: Failed to parse line '#{line}'" unless matched
 
-    @command_type = parse_command_type(matched[1])
+    @command = matched[1]
     @arg1 = matched[2]
     @arg2 = matched[3]
   end
 
   def remove_spaces_and_comment(line)
     line.gsub(%r{//.*}, '').strip
-  end
-
-  def parse_command_type(command) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-    case command
-    when 'add' || 'sub' || 'neq' || 'eq' || 'gt' || 'lt' || 'and' || 'or' || 'not'
-      'C_ARITHMETIC'
-    when 'push'
-      'C_PUSH'
-    when 'pop'
-      'C_POP'
-    else
-      raise "[Parser error]: Invalid command type '#{command}'"
-    end
   end
 end
